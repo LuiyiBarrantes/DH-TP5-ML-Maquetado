9e9045e40eda9c64e307e01113f2d9332bbf0fc7
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-01-06 02:21:08 -0300

    agrega register.html y archivos css

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const express = require ("express")
-const path = require ("path")
-const port=3030
-const app= express()
+const express = require("express")
+const path = require("path")
+const port = 3030
+const app = express()
 
 
 app.use(express.static(path.resolve(__dirname, "public")))
 
 
 
-app.listen(port,()=> console.log(`Servidor corriendo en https://localhost:${port}`))
+app.listen(port, () => console.log(`Servidor corriendo en https://localhost:${port}`))
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "views", "home.html"))
+})
+app.get("/registro", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views","user", "register.html"))
 })
